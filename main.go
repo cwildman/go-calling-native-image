33c12dc717b719997f17fcee9e6b990c330df014
Chris Wildman <cwildman@newrelic.com>
2024-08-07 22:45:49 -0700

    Attach thread within entrypoint to fix concurrency issues.

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ package main
 // #include <libnative.h>
 import "C"
 import (
-	"errors"
 	"log"
 	"os"
 	"runtime"
@@ -15,26 +14,9 @@ import (
 
 var Pnr = new(runtime.Pinner)
 
-func getOrAttachThread(isolate *C.graal_isolate_t) (*C.graal_isolatethread_t, error) {
-	var thread *C.graal_isolatethread_t = C.graal_get_current_thread(isolate)
-	if thread == nil {
-		Pnr.Pin(&thread)
-		// According to docs this is idempotent
-		if C.graal_attach_thread(isolate, &thread) != 0 {
-			return nil, errors.New("could not attach thread")
-		}
-	}
-	return thread, nil
-}
-
 func invoke(isolate *C.graal_isolate_t, wg *sync.WaitGroup) {
 	defer wg.Done()
-	var thread, err = getOrAttachThread(isolate)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	C.increment(thread)
+	C.increment(isolate)
 	return
 }
 
@@ -62,7 +44,7 @@ func main() {
 	}
 	wg.Wait()
 
-	result := C.get(thread)
+	result := C.get(isolate)
 
 	if result != C.int(count) {
 		log.Fatalf("result(%d) does not match expected count(%d)\n", result, count)
--- src/main/java/com/chriswildman/Native.java
+++ src/main/java/com/chriswildman/Native.java
@@ -1,6 +1,10 @@
 package com.chriswildman;
 
-import org.graalvm.nativeimage.IsolateThread;
+import com.oracle.svm.core.Uninterruptible;
+import com.oracle.svm.core.c.function.CEntryPointActions;
+import com.oracle.svm.core.c.function.CEntryPointOptions;
+import com.oracle.svm.core.util.VMError;
+import org.graalvm.nativeimage.Isolate;
 import org.graalvm.nativeimage.c.function.CEntryPoint;
 
 import java.util.concurrent.atomic.AtomicInteger;
@@ -8,13 +12,47 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class Native {
     public static AtomicInteger COUNT = new AtomicInteger(0);
 
+    @Uninterruptible(reason = "unsafe state in case of failure", calleeMustBe = false)
     @CEntryPoint(name = "increment")
-    public static int increment(final IsolateThread thread) {
-        return COUNT.addAndGet(1);
+    @CEntryPointOptions(prologue = CEntryPointOptions.NoPrologue.class, epilogue = CEntryPointOptions.NoEpilogue.class)
+    public static int increment(final Isolate isolate) {
+        int status = getOrAttachAndEnter(isolate);
+        if (status != 0) {
+            return status;
+        }
+
+        COUNT.addAndGet(1);
+
+        return CEntryPointActions.leave();
     }
 
+    @Uninterruptible(reason = "unsafe state in case of failure", calleeMustBe = false)
     @CEntryPoint(name = "get")
-    public static int get(final IsolateThread thread) {
-        return COUNT.get();
+    @CEntryPointOptions(prologue = CEntryPointOptions.NoPrologue.class, epilogue = CEntryPointOptions.NoEpilogue.class)
+    public static int get(final Isolate isolate) {
+        int status = getOrAttachAndEnter(isolate);
+        if (status != 0) {
+            return status;
+        }
+
+        int rv = COUNT.get();
+        CEntryPointActions.leave();
+        return rv;
+    }
+
+    @Uninterruptible(reason = "unsafe state in case of failure", calleeMustBe = false)
+    private static int getOrAttachAndEnter(final Isolate isolate) {
+        try {
+            final boolean attached = CEntryPointActions.isCurrentThreadAttachedTo(isolate);
+            int status;
+            if (!attached) {
+                status = CEntryPointActions.enterAttachThread(isolate, false, true);
+            } else {
+                status = CEntryPointActions.enterByIsolate(isolate);
+            }
+            return status;
+        } catch (final Throwable t) {
+            throw VMError.shouldNotReachHere(t);
+        }
     }
 }
